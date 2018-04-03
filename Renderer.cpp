e49d1d82192e4e5e9faf27d2f9f497fee5b9b790
Dustin Lee <guru@dustinlee.me>
2018-04-03 18:25:08 +0900

    Add files via upload
    선 그리기

--- Renderer.cpp
+++ Renderer.cpp
@@ -1,5 +1,6 @@
 
 #include "stdafx.h"
+#include "Vector.h"
 #include "SoftRenderer.h"
 #include "GDIHelper.h"
 #include "Renderer.h"
@@ -21,6 +22,22 @@ void PutPixel(int x, int y)
 	*(dest + offset) = g_CurrentColor;
 }
 
+void DrawLine(const Vector3& start, const Vector3& end)
+{
+	float length = (end - start).Dist();
+	float inc = 1.0f / length;
+
+	int maxLength = (float)length;
+	for (int i = 0; i <= maxLength; i++)
+	{
+		float t = inc * i;
+		if (t >= length) t = 1.0f;
+		Vector3 Pt = start * (1.0f - t) + end * t;
+		PutPixel(Pt.X, Pt.Y);
+	}
+
+}
+
 
 void UpdateFrame(void)
 {
@@ -29,8 +46,57 @@ void UpdateFrame(void)
 	Clear();
 
 	// Draw
+	Vector3 Pt1, Pt2;
+	Pt1.SetPoint(20.0f, 20.0f);
+	Pt2.SetPoint(160.0f, 160.0f);
+
 	SetColor(255, 0, 0);
-	PutPixel(0, 0);
+	DrawLine(Pt1, Pt2);
+
+	// 지난 수업
+	//SetColor(255, 0, 0);
+	//PutPixel(0, 0);
+
+	//static float angle = 0.0f;
+	//angle += 0.1f;
+
+	//int length = 100;
+	//Matrix2 RMat;
+	//RMat.SetRotation(angle);
+
+	//for (int i = 0; i < length; i++)
+	//{
+	//	for (int j = 0; j < length; j++)
+	//	{
+	//		if (i == j)
+	//		{
+	//			Vector2 CurrentPos((float)i, (float)j);
+	//			Vector2 NewPos = CurrentPos * RMat;
+	//			PutPixel(NewPos.X, NewPos.Y);
+	//		}
+	//	}
+	//}
+
+	// 지지난 수업
+	//Vector2 Center(0.0f, 0.0f);
+	//float radius = 100.0f;
+	//int nradius = (int)radius;
+
+	//Matrix2 Scale;
+	//Scale.SetScale(2.0f, 1.0f);
+
+	//for (int i = -nradius; i < nradius; i++)
+	//{
+	//	for (int j = -nradius; j < nradius; j++)
+	//	{
+	//		Vector2 CurrentPos(i, j);
+	//		if (Vector2::DistSquared(Center, CurrentPos) < radius * radius)
+	//		{
+	//			Vector2 newPos = CurrentPos * Scale;
+	//			PutPixel(newPos.X, newPos.Y);
+	//		}
+	//	}
+	//}
 
 	// Buffer Swap 
 	BufferSwap();
--- Vector.h
+++ Vector.h
@@ -1,4 +1,64 @@
 #pragma once
 
+#include "stdafx.h"
 #include "Matrix.h"
 
+struct Vector2
+{
+public:
+	float X;
+	float Y;
+
+public:
+	Vector2() : X(0), Y(0) {}
+	Vector2(float InX, float InY) : X(InX), Y(InY) {}
+
+	static float Dist(const Vector2 &V1, const Vector2 &V2);
+	static float DistSquared(const Vector2 &V1, const Vector2 &V2);
+
+	Vector2 operator *(const Matrix2 Mat) const;
+	bool Equals(const Vector2& V, float Tolerance = KINDA_SMALL_NUMBER) const;
+
+};
+
+FORCEINLINE bool Vector2::Equals(const Vector2& V, float Tolerance) const
+{
+	return fabs(X - V.X) <= Tolerance && fabs(Y - V.Y) <= Tolerance;
+}
+
+struct Vector3
+{
+public:
+	float X;
+	float Y;
+	float Z;
+
+	void SetPoint(float InX, float InY)
+	{
+		X = InX;
+		Y = InY;
+		Z = 1.0f;
+	}
+
+	void SetVector(float InX, float InY)
+	{
+		X = InX;
+		Y = InY;
+		Z = 0.0f;
+	}
+
+	float Dist()
+	{
+		return sqrtf(DistSquared());
+	}
+
+	float DistSquared()
+	{
+		return X * X + Y * Y;
+	}
+
+	Vector3 operator*(float scale) const;
+	Vector3 operator+(const Vector3& V) const;
+	Vector3 operator-(const Vector3& V) const;
+	Vector3 operator *(const Matrix3 Mat) const;
+};
